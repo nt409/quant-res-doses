2136891e19a7cc06d9e80ee2b63bbbb2d81fe1d6
nt409 <nt409@cam.ac.uk>
2022-09-05 10:15:16 +0100

    ok

---
+++ src/cluster/fig2_pp_cumulative.py
@@ -0,0 +1,49 @@
+"""Combine dataframes from cluster run"""
+
+import numpy as np
+import pandas as pd
+
+# N_ITS = 99
+# final one has no entries
+N_ITS = 5
+
+
+def combine():
+
+    combined = pd.concat([
+        pd.read_csv(
+            f'../outputs/f2/run_{ii}.csv')
+        for ii in range(N_ITS)
+    ]).reset_index(drop=True)
+
+    print(combined.shape)
+    print(combined.head(20))
+
+    out = (
+        combined
+        .groupby(['s', 'r', 'dose'])
+        .apply(lambda df: pd.DataFrame(
+            dict(
+                year=df.year,
+                r=df.r,
+                s=df.s,
+                dose=df.dose,
+                cumyld=np.cumsum(df.yld),
+            )
+        ))
+        .groupby(['s', 'r', 'year'])
+        .apply(lambda x: x.loc[x.yld.idxmax()])
+        .reset_index(drop=True)
+    )
+
+    print(out.head(20))
+
+    fn = '../outputs/combined/fig2_cumyld.csv'
+    print(f'saving to {fn}')
+    out.to_csv(fn, index=False)
+
+    return None
+
+
+if __name__ == "__main__":
+    combine()
